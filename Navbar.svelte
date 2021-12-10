61a99f19c3b50db8912816d11729670f3acff2a0
Hyndhavi sibbala <69784320+hyndhavigithub@users.noreply.github.com>
2021-12-10 21:46:56 +0530

    Add files via upload

---
+++ Navbar.svelte
@@ -0,0 +1,18 @@
+<div>
+    <h2>URL Shortener</h2>
+</div>
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src="https://www.parsgreen.com/content/files/Media/Image/sms/Link-Shortener.jpg">
+<style>
+    h2{
+        text-align: center;
+        background-color: rgb(245, 216, 240);
+        color: black;
+        margin: -8px;
+        padding: 15px;
+    }
+    img{
+        width: 300px;
+        margin-top: 50px;
+    }
+</style>
---
+++ index.svelte
@@ -0,0 +1,43 @@
+<script>
+import Navbar from "$lib/components/Navbar.svelte";
+import axios from 'axios'
+let url=" "
+let shorturl = "https://trim-url.vercel.app/";
+async function sendReq (){
+    const response = await axios.post("https://url-shortener-manitej66.azurewebsites.net/api/shorten?code=USDsXCIeE0UKKJTi9pMWHTyQOFWIu2zAOmWmzcAW7PorrkqDeOcnHQ==",{"url":url});
+    shorturl =shorturl + response.data.shortUrl;
+}
+</script>
+<Navbar/>
+<div class="main">
+    <input bind:value={url} type="text" placeholder="enter long url" alt=" ">
+    <button on:click={sendReq}>SHORTEN &#9989;</button>
+    <h4>CLICK HERE &#10164;</h4>
+    <a href = "{url}">{shorturl}</a>
+</div>
+<style>
+    .main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 5px;
+        flex-direction: column;
+    }
+    input{
+        padding: 20px;
+        width: 40%;
+        border-radius: 15px;
+        font-size: 15px;
+        background-color: rgb(234, 255, 149);
+        border: none;
+        outline: none;
+    }
+    button{
+        padding: 15px;
+        margin-top: 20px;
+        background-color: black;
+        color: burlywood;
+        border-radius:10px;
+        border: none;
+    }
+</style>
